a9b06c189ab580d8818355616cf14f99adadb5d1
twiwter <alexander.karadyaur@gmail.com>
2023-07-12 16:18:25 +0200

    Updated lesson 1

--- lesson_1/src/main/java/org/example/Main.java
+++ lesson_1/src/main/java/org/example/Main.java
@@ -7,9 +7,13 @@ import java.time.LocalDate;
 
 public class Main {
 
+    public static void startEngine(String brand, Worker driver){
+        System.out.println(driver.firstName + " сел в " + brand);
+    }
+
     public static void main(String[] args) {
 
-        int a1 = 10;
+        /*int a1 = 10;
         int a2 = 10;
 
         String eq = a1 == a2 ? "ДА" : "НЕТ";
@@ -24,12 +28,14 @@ public class Main {
         w.salary = 10000;
         w.date = LocalDate.of(2004, 2, 3);
 
-        Car wCar = new Car();
+        startEngine("BMW", w);
 
+        Car wCar = new Car();
         wCar.brand = "BMW";
         wCar.carName = "M5 F90";
         wCar.vin = "WBANJFLDF53940";
         wCar.owner = w;
+
 
         w.setAge(20);
 
@@ -53,7 +59,38 @@ public class Main {
         System.out.println("Owner: " + wCar.owner.firstName);
 
         System.out.println(wCar.startEngine());
-        System.out.println(wCar.stopEngine());
+        System.out.println(wCar.stopEngine());*/
+
+
+        /*char x = 'r';
+        String s = "qwerty";
+        System.out.println(s.toUpperCase());
+        char c = s.charAt(2);
+        System.out.println(c);*/
+
+        /*int a = 859;
+        int d1 = a / 100;
+        int d2 = (a / 10) % 10;
+        int d3 = a % 10;*/
+
+//        System.out.println(d3);
+
+       /* double a = 11;
+        double b = 2;
+
+        double res = a + b;
+        System.out.println(res);
+        res = a - b;
+        System.out.println(res);
+        res = a / b;
+        System.out.println(res);
+        res = a * b;
+        System.out.println(res);*/
+
+        short b1 = (short) 255;
+        short b2 = (short)1;
+
+        System.out.println(b1 + b2);
     }
 
 }
